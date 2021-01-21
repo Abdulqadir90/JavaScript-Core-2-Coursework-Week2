2c82c2f84ee85241721378f15d3f39b918f2e907
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-01-21 03:14:48 +0000

    B yet to complete

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+for (let i = 0; i < arrayOfPeople.length; i++) {
+  let firstHeader = document.createElement("h1");
+  firstHeader.innerHTML = arrayOfPeople[i]["name"];
+  content.append(firstHeader);
+  let secondHeader = document.createElement("h2");
+  secondHeader.innerHTML = arrayOfPeople[i]["job"];
+  content.append(secondHeader);
+
+}
 }
 
 let people = [
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,19 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfLists) {
+  let content = document.querySelector("content");
+
+  for (let i =0; i < arrayOfLists.length; i++) {
+    let newList = document.createElement("ul");
+    newList.innerHTML = arrayOfLists[i];
+    content.append(newList);
+    let itemsEl = document.createElement("li");
+    itemsEl.innerHTML = arrayOfLists[i];
+    newList.append(itemsEl);
+  }
+
+
+  
+
+
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
