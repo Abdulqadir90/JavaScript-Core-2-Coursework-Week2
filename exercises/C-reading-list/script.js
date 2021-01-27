c0536e455c196fdee290414f1d14ab55c3017b68
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-01-27 17:25:34 +0000

    ...

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,21 +1,57 @@
 function readingList(books) {
-  // Write your code here...
+  let content = document.querySelector("content");
+  let listEl = document.createElement("ul");
+  content.appendChild(listEl);
+
+  books.forEach(function (obj) {
+    let el = document.createElement("li");
+    listEl.appendChild(el);
+
+    let titleEl = document.createElement("p");
+    titleEl.innerHTML = obj["newTitle"];
+    el.appendChild(titleEl);
+
+    el.style.height = "50px";
+    el.style.width = "50px";
+
+    let authorEl = document.createElement("p");
+    authorEl.innerHTML = obj["author"];
+    el.appendChild(authorEl);
+
+    let imgEl = document.createElement("img");
+    imgEl.src = obj["image"];
+    imgEl.style.width = "50%";
+    el.appendChild(imgEl);
+
+    if (obj["alreadyRead"]) {
+      el.style.backgroundColor === "green";
+    } else {
+      el.style.backgroundColor ==="red";
+
+    }
+
+  });
 }
+
+
 
 const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
+    image: "https://images-na.ssl-images-amazon.com/images/I/410RTQezHYL._SX326_BO1,204,203,200_.jpg" ,
     alreadyRead: false
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
+    image: "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg" ,
     alreadyRead: true
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
+    image: "https://pragprog.com/titles/tpp20/the-pragmatic-programmer-20th-anniversary-edition/tpp20.jpg" , 
     alreadyRead: true
   }
 ];
--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,6 +1,24 @@
 function todoList(todos) {
-  // Write your code here...
+
+  let content = document.querySelector("content");
+  let listEl = document.createElement("ul");
+
+  todos.forEach(el => {
+    let listEls = document.createElement("li");
+    listEls.innerText = el.todo;
+    listEls.addEventListener("click", event => { ;
+    event.preventDefault();
+    if (listEls.style.textDecoration ==="line-through"){
+      listEls.style.textDecoration = "none"
+    } else {
+      listEls.style.textDecoration = "line-through";
+    }
+  })
+listEl.appendChild(listEls);
+  })
+  content.appendChild(listEls);
 }
+
 
 const todos = [
   {todo: "wash the dishes"}, 
